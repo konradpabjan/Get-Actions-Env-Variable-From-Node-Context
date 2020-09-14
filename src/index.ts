706196efccbb26cf8e6c0a68ae6a9805ca99b024
konradpabjan <konradpabjan@github.com>
2020-09-14 13:20:06 -0400

    testig

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 const chalk = require('chalk')
 
-function run(){
+async function run(){
 
-    const envVariable = core.getInput('EnvName', { required: true });
-    const envValue = process.env[envVariable]
+    //const envVariable = core.getInput('EnvName', { required: true });
+    //const envValue = process.env[envVariable]
 
     console.log("this is going to be some normal output")
     core.info("this is output using core.info")
@@ -17,8 +18,6 @@ function run(){
     core.error("This is what standard error looks like")
     core.warning("This is going to be standard warning output")
 
-    chalk.bgCyan
-
     console.log("some normal output again")
 
     console.log('curious about this' + chalk.blue("Blue with console log"))
@@ -26,11 +25,15 @@ function run(){
     console.log("normal output that should reset color")
     core.info(chalk.blue("Blue cyan with core info"))
 
+    await exec.exec('bash', ['-c','curl -s https://gist.githubusercontent.com/HaleTom/89ffe32783f89f403bba96bd7bcd1263/raw/ | bash'])
+
+    /*
     if(!envValue){
         core.setFailed(`Unable to get the value of the ${envVariable} env variable`)
     } else {
         core.setOutput('EnvValue', envValue)
     }
+    */
 }
 
 run()
